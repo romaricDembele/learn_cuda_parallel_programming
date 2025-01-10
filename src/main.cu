e2700891d33919d161f64b19cacd561d391050ea
Romaric DEMBELE <romadembele@protonmail.com>
2025-01-10 11:50:36 +0100

    Correct typo + memory names.

--- src/main.cu
+++ src/main.cu
@@ -26,44 +26,44 @@ void addition() {
     // Dimension of the vectors to be added
     int DIM = 8;
 
-    // Declare pointers on the CPU memoru for the vectors
+    // Declare pointers on the HOST memory for the vectors
     // to be added (A and B) and the vector holding the result
     int* A;
     int* B;
     int* C;
 
-    // Declare pointers on the CPU memory to hold GPU addresses 
+    // Declare pointers on the HOST memory to hold GPU addresses 
     // of the two vectors to be added.
     int* device_A;
     int* device_B;
     int* device_C;
 
-    // Allocate memory on the CPU memory heap for the tow 
+    // Allocate memory on the HOST memory heap for the tow 
     // vectors to be added
     A = (int *)malloc(DIM * sizeof(int));
     B = (int *)malloc(DIM * sizeof(int));
     C = (int *)malloc(DIM * sizeof(int));
 
-    // Allocate memory on the GPU memory heap for the tow 
+    // Allocate memory on the DEVICE memory heap for the tow 
     // vectors to be added
     cudaMalloc((void **)&device_A, DIM * sizeof(int));
     cudaMalloc((void **)&device_B, DIM * sizeof(int));
     cudaMalloc((void **)&device_C, DIM * sizeof(int));
 
-    // Initialize the vectors to be added in the CPU memory
+    // Initialize the vectors to be added in the HOST memory
     for (int i = 0; i < DIM; i++) {
         A[i] = i;
         B[i] = DIM - i;
     }
 
-    // Copy the vectors to the GPU memory
+    // Copy the vectors to the DEVICE memory
     cudaMemcpy(device_A, A, DIM * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(device_B, B, DIM * sizeof(int), cudaMemcpyHostToDevice);
 
     // Call the Addition Kernel function
     addVectors<<<1, DIM>>>(device_A, device_B, device_C);
 
-    // Copy the result back to the CPU memory
+    // Copy the result back to the HOST memory
     cudaMemcpy(C, device_C, DIM * sizeof(DIM), cudaMemcpyDeviceToHost);
  
     // Ensure that the CPU wait for the GPU workers to finish
